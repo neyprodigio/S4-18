15f52e6d7582a0c6311747f70739d50a9a6e9301
Neilson Oliveira <106606239+neyprodigio@users.noreply.github.com>
2022-06-22 20:32:37 -0300

    S4-18 JS

---
+++ script.js
@@ -0,0 +1,91 @@
+function oneThroughTwenty(){
+    
+    let meuRetorno = [];
+    for(let i=1; i<=20; i++){
+        meuRetorno.push(i)
+    }
+    return meuRetorno
+}
+console.log(oneThroughTwenty())
+
+/* function evensToTwenty(){
+    
+    let meuRetorno = [];
+    for(let i=2; i<=20; i+=2){
+        meuRetorno.push(i)
+    }
+    return meuRetorno
+}
+console.log(evensToTwenty()) */
+
+/* function oddsToTwenty(){
+    
+    let meuRetorno = [];
+    for(let i=1; i<=19; i+=2){
+        meuRetorno.push(i)
+    }
+    return meuRetorno
+}
+console.log(oddsToTwenty()) */
+
+/* function squareNumbers(){
+    
+    let meuRetorno = [];
+    
+    for(let i=1; i<=10; i++) {
+        let quadrado = i * i;
+        meuRetorno.push(quadrado) 
+    }
+    return meuRetorno;
+}
+console.log(squareNumbers()) */
+
+/* function countingBackwards(){
+    let i=20;
+    let meuRetorno = [];
+    for(i; i>1; i--){
+        meuRetorno.push(i)
+    }
+    return meuRetorno
+}
+console.log(countingBackwards()) */
+
+/* function evenNumbersBackwards(){
+    let meuRetorno = [];
+    for(let i=20; i>=2; i-=2) {
+        meuRetorno.push(i)
+    }
+    return meuRetorno
+}
+console.log(evenNumbersBackwards()) */
+
+/* function oddNumbersBackwards(){
+    let meuRetorno = [];
+    
+    for(let i=19; i>=1; i-=2){
+        meuRetorno.push(i)
+    }
+    return meuRetorno
+}
+console.log(oddNumbersBackwards()) */
+
+/* function multiplesOfFiveBackwards() {
+    let meuRetorno = [];
+    for(let i=100; i>=5; i-=5){
+        meuRetorno.push(i)
+    }
+    return meuRetorno
+}
+console.log(multiplesOfFiveBackwards()) */
+
+
+/* function squareNumbers(){
+    let meuRetorno= [];
+
+    for(let i=10; i>=1; i--) {
+        let quadrado = i * i;
+        meuRetorno.push(quadrado) ;
+    }
+    return meuRetorno;
+}
+console.log(squareNumbers()) */
